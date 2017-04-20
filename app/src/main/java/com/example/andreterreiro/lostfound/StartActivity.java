bbd5f4ead6ad0d4a822fce78c92012fe2aee81b2
andreterreiro <andreterreiro94@gmail.com>
2017-04-20 17:46:36 +0100

    1/2 login

--- app/src/main/java/com/example/andreterreiro/lostfound/StartActivity.java
+++ app/src/main/java/com/example/andreterreiro/lostfound/StartActivity.java
@@ -4,9 +4,15 @@ import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,14 +21,16 @@ public class StartActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "Firebase";
+    private EditText user;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        EditText user = (EditText) findViewById(R.id.txtMail);
-        EditText pass = (EditText) findViewById(R.id.txtPassword);
+        user = (EditText) findViewById(R.id.txtMail);
+        pass = (EditText) findViewById(R.id.txtPassword);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -55,4 +63,73 @@ public class StartActivity extends AppCompatActivity {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = user.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            user.setError("Required.");
+            valid = false;
+        } else {
+            user.setError(null);
+        }
+
+        String password = pass.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            pass.setError("Required.");
+            valid = false;
+        } else {
+            pass.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void signIn(View view) {
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(StartActivity.this, "ERRO LOGIN", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signUp(View view) {
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(StartActivity.this, "ERRO REGISTO", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
